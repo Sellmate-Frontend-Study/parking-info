d05485b72d9bf6431a53ad892525ac8255860fc7
EunJin Kim <eunjin9639@gmail.com>
2025-03-06 23:28:00 +0900

    현재 위치를 기반으로 카카오 지도의 중심 좌표 설정

--- src/hooks/useKakaoMap.ts
+++ src/hooks/useKakaoMap.ts
@@ -8,16 +8,18 @@ const useKakaoMap = () => {
   if (!isKakaoLoaded) return; // SDK가 로드되지 않으면 실행 안 함
 
   kakao.maps.load(() => {
-   if (!mapEl.current) {
-    console.error("지도를 표시할 div(#map)를 찾을 수 없음");
-    return;
-   }
-   const mapOption = {
-    center: new window.kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3,
-   };
+   if (!navigator.geolocation) return
+   navigator.geolocation.getCurrentPosition((position) => {
+    if (!mapEl.current) return
 
-   new window.kakao.maps.Map(mapEl.current, mapOption);
+    const { latitude, longitude } = position.coords
+    const mapOption = {
+     center: new window.kakao.maps.LatLng(latitude, longitude),
+     level: 3,
+    };
+
+    new window.kakao.maps.Map(mapEl.current, mapOption);
+   })
   })
  }, [isKakaoLoaded]); // SDK가 로드된 후 실행
 
