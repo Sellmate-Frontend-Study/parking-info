3ad9d801d16ccba719bc389c74b201a0e8db1f04
EunJin Kim <eunjin9639@gmail.com>
2025-03-17 21:21:50 +0900

    KakaoMap 컴포넌트 추가 및 페이지에서 사용하도록 수정

---
+++ src/app/_components/KakaoMap.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import useKakaoMap from '@/hooks/useKakaoMap';
+import Script from 'next/script';
+
+const KakaoMap = () => {
+ const { mapEl, setIsKakaoLoaded } = useKakaoMap();
+
+ return <>
+  <Script
+   src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_MAP_KEY}&libraries=services&autoload=false`}
+   strategy="afterInteractive"
+   onLoad={() => {
+    setIsKakaoLoaded(true);
+   }}
+  />
+  <main ref={mapEl} className='h-screen'></main>
+ </>
+
+}
+export default KakaoMap
