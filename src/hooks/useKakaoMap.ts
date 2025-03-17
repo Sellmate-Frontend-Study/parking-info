ce276eaa0c9fd723b0f77162b7576a8544088212
EunJin Kim <eunjin9639@gmail.com>
2025-03-17 23:12:29 +0900

    KakaoMap의 중심 좌표를 서울 시청으로 설정하고, 현재 위치 기반 로드 코드를 주석 처리

--- src/hooks/useKakaoMap.ts
+++ src/hooks/useKakaoMap.ts
@@ -4,22 +4,27 @@ const useKakaoMap = () => {
  const mapEl = useRef<HTMLElement | null>(null)
  const [isKakaoLoaded, setIsKakaoLoaded] = useState(false)
 
+ // 서울 시청 좌표
+ const LATITUDE = 37.5665
+ const LONGITUDE = 126.978
  useEffect(() => {
   if (!isKakaoLoaded) return; // SDK가 로드되지 않으면 실행 안 함
 
   kakao.maps.load(() => {
-   if (!navigator.geolocation) return
-   navigator.geolocation.getCurrentPosition((position) => {
-    if (!mapEl.current) return
+   // 실시간 정보
+   // if (!navigator.geolocation) return
+   // navigator.geolocation.getCurrentPosition((position) => {
 
-    const { latitude, longitude } = position.coords
-    const mapOption = {
-     center: new window.kakao.maps.LatLng(latitude, longitude),
-     level: 3,
-    };
+   //  const { latitude, longitude } = position.coords
+   // })
 
-    new window.kakao.maps.Map(mapEl.current, mapOption);
-   })
+   if (!mapEl.current) return
+   const mapOption = {
+    center: new window.kakao.maps.LatLng(LATITUDE, LONGITUDE),
+    level: 3,
+   };
+
+   new window.kakao.maps.Map(mapEl.current, mapOption);
   })
  }, [isKakaoLoaded]); // SDK가 로드된 후 실행
 
