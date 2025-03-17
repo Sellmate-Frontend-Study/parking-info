125789a1bc8013fbe69f1c355d94cd453f80900d
EunJin Kim <eunjin9639@gmail.com>
2025-03-17 23:12:24 +0900

    주차 정보 API 추가 및 KakaoMap에서 데이터 가져오기 기능 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
-import KakaoMap from './_components/KakaoMap';
+"use client"
+
+import { useEffect } from 'react';
+import KakaoMap from '@/components/KakaoMap';
+import type { ParkingInfo } from './api/parking-info/route';
 
 export default function Home() {
+ const getParkingInfo = async () => {
+  try {
+   const res = await fetch('api/parking-info')
+   const json: { data: ParkingInfo[] } = await res.json();
+   console.log(json)
+  } catch (error) {
+   console.error(error)
+  }
+ }
+
+ useEffect(() => {
+  getParkingInfo()
+ }, [])
+
  return (
   <KakaoMap />
  )
