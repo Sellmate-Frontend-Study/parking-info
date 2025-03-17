96fca24a1058f2b933aca9b2252dd91b653b931d
yang <diddnjswns20@gmail.com>
2025-03-18 01:04:23 +0900

    FEAT: Parking data 불러오기

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,35 +1,76 @@
 'use client';
 
 import Script from 'next/script';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { getParkingInfo } from '../actions/getParkingInfo';
 
 declare global {
   interface Window {
     kakao: any;
   }
 }
+const MY_LAT = 37.5665;
+const MY_LNG = 126.978;
+const DEFAULT_RADIUS = 250;
 
 const KakaoMap = () => {
   const mapRef = useRef<HTMLDivElement>(null);
+  const [centerPosition, setCenterPosition] = useState({
+    lat: MY_LAT,
+    lng: MY_LNG
+  });
+  const [radius, setRadius] = useState(DEFAULT_RADIUS);
+
+  const fetchParkingInfo = async () => {
+    try {
+      const res = await getParkingInfo(
+        centerPosition.lat,
+        centerPosition.lng,
+        radius
+      );
+      console.log(res);
+    } catch (error) {
+      console.error('fetchParkingInfo Error', error);
+    }
+  };
 
   const initializeMap = () => {
     if (mapRef.current && window.kakao) {
-      window.kakao.maps.load(() => {
+      window.kakao.maps.load(() => {  
+        const position = new window.kakao.maps.LatLng(centerPosition.lat, centerPosition.lng);
+        
         const options = {
-          center: new window.kakao.maps.LatLng(37.5665, 126.978),
+          center: position,
           level: 3
         };
-        new window.kakao.maps.Map(mapRef.current, options);
+        
+        const kakaoMap = new window.kakao.maps.Map(mapRef.current, options);
+        const marker = new window.kakao.maps.Marker({
+          position: position,
+          content: `<div style="background-color: red; width: 12px; height: 12px; border-radius: 50%;"></div>`,
+        });
+        
+        const circle = new window.kakao.maps.Circle({
+          center: position,
+          radius: radius,
+          strokeColor: '#75B8FA',
+          fillColor: '#CFE7FF',
+          fillOpacity: 0.3
+        });
+        marker.setMap(kakaoMap);
+        circle.setMap(kakaoMap);
+        
+        fetchParkingInfo();
       });
     }
   };
 
   return (
-    <>
+    <div className="relative w-full h-full">
       <Script
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`}
         onLoad={initializeMap}
-      />
+      />      
       <div
         ref={mapRef}
         style={{
@@ -37,7 +78,7 @@ const KakaoMap = () => {
           height: '100%'
         }}
       />
-    </>
+    </div>
   );
 };
 
