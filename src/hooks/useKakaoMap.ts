dc4e32a997886f9a6461d23d3d40a4f996ef4bab
EunJin Kim <eunjin9639@gmail.com>
2025-03-30 21:26:47 +0900

    useKakaoMap 훅에서 반경을 매개변수로 받아 사용하도록 수정

--- src/hooks/useKakaoMap.ts
+++ src/hooks/useKakaoMap.ts
@@ -1,51 +1,50 @@
 import { useEffect, useState } from 'react';
 
-const useKakaoMap = () => {
-	const RADIUS = 250;
-	const [centerLocation, setCenterLocation] = useState({ lat: 37.5665, lng: 126.978 });
-	const [map, setMap] = useState<kakao.maps.Map | null>(null);
-	const [circle, setCircle] = useState<kakao.maps.Circle | null>(null);
+const useKakaoMap = (radius = 250) => {
+ const [centerLocation, setCenterLocation] = useState({ lat: 37.5665, lng: 126.978 });
+ const [map, setMap] = useState<kakao.maps.Map | null>(null);
+ const [circle, setCircle] = useState<kakao.maps.Circle | null>(null);
 
-	const initMap = (mapElement: HTMLElement) => {
-		kakao.maps.load(() => {
-			const center = new kakao.maps.LatLng(centerLocation.lat, centerLocation.lng);
-			const options = {
-				center: center,
-				level: 3,
-			};
+ const initMap = (mapElement: HTMLElement) => {
+  kakao.maps.load(() => {
+   const center = new kakao.maps.LatLng(centerLocation.lat, centerLocation.lng);
+   const options = {
+    center: center,
+    level: 3,
+   };
 
-			const map = new kakao.maps.Map(mapElement, options);
-			setMap(map);
+   const map = new kakao.maps.Map(mapElement, options);
+   setMap(map);
 
-			const circle = new kakao.maps.Circle({
-				center: center,
-				radius: RADIUS,
-				strokeColor: '#75B8FA',
-				fillColor: '#CFE7FF',
-				fillOpacity: 0.3,
-			});
-			circle.setMap(map);
-			setCircle(circle);
+   const circle = new kakao.maps.Circle({
+    center: center,
+    radius,
+    strokeColor: '#75B8FA',
+    fillColor: '#CFE7FF',
+    fillOpacity: 0.3,
+   });
+   circle.setMap(map);
+   setCircle(circle);
 
-			kakao.maps.event.addListener(map, 'dragend', () => {
-				const position = map.getCenter();
-				setCenterLocation({ lat: position.getLat(), lng: position.getLng() });
-			});
-		});
-	};
+   kakao.maps.event.addListener(map, 'dragend', () => {
+    const position = map.getCenter();
+    setCenterLocation({ lat: position.getLat(), lng: position.getLng() });
+   });
+  });
+ };
 
-	const setCirclePosition = () => {
-		if (circle) {
-			const moveLocation = new kakao.maps.LatLng(centerLocation.lat, centerLocation.lng);
-			circle.setPosition(moveLocation);
-		}
-	};
+ const setCirclePosition = () => {
+  if (circle) {
+   const moveLocation = new kakao.maps.LatLng(centerLocation.lat, centerLocation.lng);
+   circle.setPosition(moveLocation);
+  }
+ };
 
-	useEffect(() => {
-		setCirclePosition();
-	}, [centerLocation]);
+ useEffect(() => {
+  setCirclePosition();
+ }, [centerLocation]);
 
-	return { map, RADIUS, centerLocation, initMap };
+ return { map, centerLocation, initMap };
 };
 
 export default useKakaoMap;
