c1d390e2245e65d6b00512ad50377be2daa8f84d
EunJin Kim <eunjin9639@gmail.com>
2025-03-30 21:26:41 +0900

    API URL 수정: BASE_URL 환경변수 사용으로 변경

--- src/actions/parkInfo.ts
+++ src/actions/parkInfo.ts
@@ -3,30 +3,30 @@
 import { ParkInfo, ParkInfoResponse } from '@/types/parkInfo';
 
 export const getParkInfo = async (): Promise<ParkInfo[]> => {
-	const pageSize = 1000;
-	const apiKey = process.env.NEXT_PUBLIC_OPEN_API_KEY;
-	const apiUrl = `http://openapi.seoul.go.kr:8088/${apiKey}/json/GetParkInfo`;
+ const pageSize = 1000;
+ const apiKey = process.env.NEXT_PUBLIC_OPEN_API_KEY;
+ const apiUrl = `${process.env.BASE_URL}/${apiKey}/json/GetParkInfo`;
 
-	const response = await fetch(`${apiUrl}/1/${pageSize}`, {
-		next: { revalidate: 3600 },
-	});
-	const json: ParkInfoResponse = await response.json();
+ const response = await fetch(`${apiUrl}/1/${pageSize}`, {
+  next: { revalidate: 3600 },
+ });
+ const json: ParkInfoResponse = await response.json();
 
-	const totalCount = json.GetParkInfo.list_total_count;
-	let parkInfos = [...json.GetParkInfo.row];
+ const totalCount = json.GetParkInfo.list_total_count;
+ let parkInfos = [...json.GetParkInfo.row];
 
-	let currentIndex = pageSize + 1;
+ let currentIndex = pageSize + 1;
 
-	while (currentIndex <= totalCount) {
-		const endIndex = Math.min(currentIndex + pageSize - 1, totalCount);
-		const response = await fetch(`${apiUrl}/${currentIndex}/${endIndex}`, {
-			next: { revalidate: 3600 },
-		});
-		const json: ParkInfoResponse = await response.json();
+ while (currentIndex <= totalCount) {
+  const endIndex = Math.min(currentIndex + pageSize - 1, totalCount);
+  const response = await fetch(`${apiUrl}/${currentIndex}/${endIndex}`, {
+   next: { revalidate: 3600 },
+  });
+  const json: ParkInfoResponse = await response.json();
 
-		parkInfos = parkInfos.concat(json.GetParkInfo.row);
-		currentIndex += pageSize;
-	}
+  parkInfos = parkInfos.concat(json.GetParkInfo.row);
+  currentIndex += pageSize;
+ }
 
-	return parkInfos;
+ return parkInfos;
 };
--- src/actions/parkingInfo.ts
+++ src/actions/parkingInfo.ts
@@ -3,26 +3,26 @@
 import { ParkingInfo, ParkingInfoResponse } from '@/types/parkingInfo';
 
 export const getParkingInfo = async (): Promise<ParkingInfo[]> => {
-	const pageSize = 1000;
-	const apiKey = process.env.NEXT_PUBLIC_OPEN_API_KEY;
-	const apiUrl = `http://openapi.seoul.go.kr:8088/${apiKey}/json/GetParkingInfo`;
+ const pageSize = 1000;
+ const apiKey = process.env.NEXT_PUBLIC_OPEN_API_KEY;
+ const apiUrl = `${process.env.BASE_URL}/${apiKey}/json/GetParkingInfo`;
 
-	const response = await fetch(`${apiUrl}/1/${pageSize}`);
-	const json: ParkingInfoResponse = await response.json();
+ const response = await fetch(`${apiUrl}/1/${pageSize}`);
+ const json: ParkingInfoResponse = await response.json();
 
-	const totalCount = json.GetParkingInfo.list_total_count;
-	let parkingInfos = [...json.GetParkingInfo.row];
+ const totalCount = json.GetParkingInfo.list_total_count;
+ let parkingInfos = [...json.GetParkingInfo.row];
 
-	let currentIndex = pageSize + 1;
+ let currentIndex = pageSize + 1;
 
-	while (currentIndex <= totalCount) {
-		const endIndex = Math.min(currentIndex + pageSize - 1, totalCount);
-		const response = await fetch(`${apiUrl}/${currentIndex}/${endIndex}`);
-		const json: ParkingInfoResponse = await response.json();
+ while (currentIndex <= totalCount) {
+  const endIndex = Math.min(currentIndex + pageSize - 1, totalCount);
+  const response = await fetch(`${apiUrl}/${currentIndex}/${endIndex}`);
+  const json: ParkingInfoResponse = await response.json();
 
-		parkingInfos = parkingInfos.concat(json.GetParkingInfo.row);
-		currentIndex += pageSize;
-	}
+  parkingInfos = parkingInfos.concat(json.GetParkingInfo.row);
+  currentIndex += pageSize;
+ }
 
-	return parkingInfos;
+ return parkingInfos;
 };
