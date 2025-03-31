323db5c7d2eeb1fa86940fbe6918d3e8638c21b6
MeeKyeong Kim <kmeijjing@gmail.com>
2025-04-01 01:14:35 +0900

    주차장 상태를 나타내는 마커 타입 설명 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import KakaoMap from '@/components/KakaoMap';
+import MarkerTypeNotice from '@/components/MarkerTypeNotice';
 import { ParkInfoProvider } from '@/providers/ParkInfoProvider';
 
 const Home = () => {
 	return (
-		<div className='h-screen w-screen'>
+		<div className='relative h-screen w-screen'>
 			<ParkInfoProvider>
 				<KakaoMap />
+				<MarkerTypeNotice />
 			</ParkInfoProvider>
 		</div>
 	);
