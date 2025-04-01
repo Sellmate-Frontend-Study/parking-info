15a73232067bbeee5ba3e5bce09139a410bfdb21
yang <diddnjswns20@gmail.com>
2025-04-01 23:52:01 +0900

    center marker

--- src/hooks/useKakaoMap.ts
+++ src/hooks/useKakaoMap.ts
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 
+declare const kakao: any;
+
 const useKakaoMap = () => {
 	const RADIUS = 250;
 	const [centerLocation, setCenterLocation] = useState({ lat: 37.5665, lng: 126.978 });
 	const [map, setMap] = useState<kakao.maps.Map | null>(null);
 	const [circle, setCircle] = useState<kakao.maps.Circle | null>(null);
+	const [marker, setMarker] = useState<kakao.maps.Marker | null>(null);
 
 	const initMap = (mapElement: HTMLElement) => {
 		kakao.maps.load(() => {
@@ -27,6 +30,18 @@ const useKakaoMap = () => {
 			circle.setMap(map);
 			setCircle(circle);
 
+			const centerMarker = new kakao.maps.Circle({
+        center: center,
+        radius: 8, 
+        strokeWeight: 3, 
+  			strokeColor: "white", 
+				strokeOpacity: 0.7,
+  			fillColor: "red", 
+  			fillOpacity: 1,
+      });
+			centerMarker.setMap(map);
+			setMarker(centerMarker);
+
 			kakao.maps.event.addListener(map, 'dragend', () => {
 				const position = map.getCenter();
 				setCenterLocation({ lat: position.getLat(), lng: position.getLng() });
@@ -41,8 +56,16 @@ const useKakaoMap = () => {
 		}
 	};
 
+	const setMarkerPosition = () => {
+		if (marker) {
+			const moveLocation = new kakao.maps.LatLng(centerLocation.lat, centerLocation.lng);
+			marker.setPosition(moveLocation);
+		}
+	};
+
 	useEffect(() => {
 		setCirclePosition();
+		setMarkerPosition(); 
 	}, [centerLocation]);
 
 	return { map, RADIUS, centerLocation, initMap };
