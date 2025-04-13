6c59b817bdbf0d161edd68d2e17299cd701330ef
kuy <kuy472013@gmail.com>
2025-04-13 17:47:37 +0900

    kakao 주소 검색 api 수정

--- src/actions/localAddress.ts
+++ src/actions/localAddress.ts
@@ -4,7 +4,7 @@ import { LocalAddress, LocalAddressResponse } from '@/types/localAddress';
 
 export const getLocalAddress = async (address: string): Promise<LocalAddress[]> => {
 	const apiKey = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
-	const apiUrl = `https://dapi.kakao.com/v2/local/search/address`;
+	const apiUrl = `https://dapi.kakao.com/v2/local/search/keyword`;
 
 	const response = await fetch(`${apiUrl}?query=${address}`, {
 		headers: {
