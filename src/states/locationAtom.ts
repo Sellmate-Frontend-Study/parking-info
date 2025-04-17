515b1c557e56fd192901c7ab5cd3039e3f189acd
kuy <kuy472013@gmail.com>
2025-04-18 00:33:10 +0900

    import 경로 수정

--- src/states/locationAtom.ts
+++ src/states/locationAtom.ts
@@ -1,4 +1,4 @@
-import { Location } from '@/types/location';
+import { Location } from '@/types/map';
 import { atom } from 'jotai';
 
 export const locationAtom = atom<Location>({ latitude: 37.5665, longitude: 126.978 });
