7b4c97535fffe5447513a81671e2ce9021ca7f93
kuy <kuy472013@gmail.com>
2025-04-30 00:49:40 +0900

    키워드 검색 응답 형태 변경

--- src/types/localAddress.ts
+++ src/types/localAddress.ts
@@ -1,35 +1,16 @@
 export interface LocalAddress {
-	addrss: {
-		address_name: string;
-		region_1depth_name: string;
-		region_2depth_name: string;
-		region_3depth_name: string;
-		h_code: string;
-		b_code: string;
-		mountain_yn: string;
-		main_address_no: string;
-		sub_address_no: string;
-		x: string;
-		y: string;
-	};
+	id: string;
+	place_name: string;
+	category_name: string;
+	category_group_code: string;
+	category_group_name: string;
+	phone: string;
 	address_name: string;
-	address_type: string;
-	road_address?: {
-		address_name: string;
-		region_1depth_name: string;
-		region_2depth_name: string;
-		region_3depth_name: string;
-		road_name: string;
-		underground_yn: string;
-		main_building_no: string;
-		sub_building_no: string;
-		building_name: string;
-		zone_no: string;
-		x: string;
-		y: string;
-	};
+	road_address: string;
 	x: string;
 	y: string;
+	place_url: string;
+	distance: string;
 }
 
 export interface LocalAddressResponse {
@@ -37,6 +18,11 @@ export interface LocalAddressResponse {
 		total_count: number;
 		pageable_count: number;
 		is_end: boolean;
+		same_name: {
+			region: string[];
+			keyword: string;
+			selected_region: string;
+		};
 	};
 	documents: LocalAddress[];
 }
