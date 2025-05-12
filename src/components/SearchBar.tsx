4554fbfe1d3ffd41196002b1e95b8bfe8889aded
kuy <kuy472013@gmail.com>
2025-05-12 20:50:03 +0900

    검색 결좌 주차장 정보로 변경

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,17 +2,22 @@
 
 import { getLocalAddress } from '@/actions/localAddress';
 import useInput from '@/hooks/useInput';
+import useParkInfo from '@/hooks/useParkInfo';
 import { locationAtom } from '@/states/locationAtom';
-import { LocalAddress } from '@/types/localAddress';
+import { radiusAtom } from '@/states/radiusAtom';
+import { ParkInfo } from '@/types/parkInfo';
 import SearchIcon from '@assets/search.svg';
 import clsx from 'clsx';
-import { useSetAtom } from 'jotai';
+import { useAtomValue, useSetAtom } from 'jotai';
 import { useState } from 'react';
+import Location from '@assets/location.svg';
 
 const SearchBar = () => {
+	const radius = useAtomValue(radiusAtom);
 	const setLocatin = useSetAtom(locationAtom);
+	const { getTargetParkInfos } = useParkInfo();
 	const { value, handleChange } = useInput();
-	const [result, setResult] = useState<Array<LocalAddress>>([]);
+	const [result, setResult] = useState<Array<ParkInfo>>([]);
 
 	const search = () => {
 		getLocalAddress(value).then((data) => {
@@ -21,7 +26,11 @@ const SearchBar = () => {
 				return;
 			}
 
-			setResult(data);
+			const targetParkInfos = getTargetParkInfos(
+				{ latitude: parseFloat(data[0].y), longitude: parseFloat(data[0].x) },
+				radius
+			);
+			setResult(targetParkInfos);
 		});
 	};
 
@@ -50,24 +59,35 @@ const SearchBar = () => {
 
 			<ul
 				className={clsx(
-					'top-14 w-md list-none overflow-hidden rounded-md border-solid border-gray-100 bg-white text-center text-black shadow-xl',
+					'scrollbar-hide top-14 h-150 w-md list-none overflow-y-scroll rounded-md border-solid border-gray-100 bg-white text-center text-black shadow-xl',
 					result.length > 0 && 'absolute',
 					result.length === 0 && 'hidden'
 				)}
 			>
-				{result.map((v) => (
+				{result.map((v, k) => (
 					<li
-						key={v.id}
-						className='h-8 content-center hover:bg-gray-100'
+						key={k}
+						className='content-center hover:bg-gray-100'
 						onClick={() => {
 							setLocatin({
-								latitude: parseFloat(v.y),
-								longitude: parseFloat(v.x),
+								latitude: v.LAT,
+								longitude: v.LOT,
 							});
 							setResult([]);
 						}}
 					>
-						{v.place_name}
+						<div className='flex flex-row items-center gap-4 px-4 py-3'>
+							<Location className='h-[20px] w-[20px] text-gray-400' />
+							<div className='flex w-full flex-row justify-between'>
+								<div className='flex flex-col items-start text-start text-sm'>
+									<span>{v.PKLT_NM}</span>
+									<span>{v.ADDR}</span>
+								</div>
+							</div>
+							<div className='w-22 self-center text-sm'>
+								<span>{v.PKLT_KND_NM}</span>
+							</div>
+						</div>
 					</li>
 				))}
 			</ul>
